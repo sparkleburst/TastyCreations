24182b0bd12b4fff46f9ac58576f2360e6e4140a
sparkleburst <maxandmerlin@gmail.com>
2024-09-16 16:07:57 -0500

    initial commit

---
+++ src/main/java/com/mgilhespy/tastycreations/TastyCreationsApplication.java
@@ -0,0 +1,13 @@
+package com.mgilhespy.tastycreations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyCreationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyCreationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgilhespy/tastycreations/TastyCreationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgilhespy.tastycreations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyCreationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
