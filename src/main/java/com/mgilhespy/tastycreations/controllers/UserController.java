d01444424fef86c7fa0532f318333bc8a4f5e6df
Irina Markov <imarkov1275@gmail.com>
2024-09-17 16:22:00 -0500

    added WEB-INF

--- src/main/java/com/mgilhespy/tastycreations/controllers/UserController.java
+++ src/main/java/com/mgilhespy/tastycreations/controllers/UserController.java
@@ -18,46 +18,46 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-//    @GetMapping("")
-//    public String index(@ModelAttribute("newUser") User newUser, @ModelAttribute("loginUser") LoginUser loginUser) {
-//        return "index";
-//    }
-//
-//    @PostMapping("/register")
-//    public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model, HttpSession session) {
-//        User user = userService.register(newUser, result);
-//
-//        if (result.hasErrors()) {
-//            model.addAttribute("loginUser", new LoginUser());
-//            model.addAttribute("savedDate", newUser.getBirthDate());
-//            return "index";
-//        }
-//        session.setAttribute("userId", user.getId());
-//        return "redirect:/dashboard";
-//    }
-//
-//    @PostMapping("/login")
-//    public String login(@Valid @ModelAttribute("loginUser") LoginUser loginUser, BindingResult result, Model model, HttpSession session) {
-//        if (result.hasErrors()) {
-//            model.addAttribute("newUser", new User());
-//            model.addAttribute("loginUser", loginUser);
-//            return "index";
-//        }
-//
-//        User potentialUser = this.userService.login(loginUser, result);
-//        if (potentialUser == null) {
-//            model.addAttribute("newUser", new User());
-//            model.addAttribute("loginUser", loginUser);
-//            return "index";
-//        }
-//
-//        session.setAttribute("userId", potentialUser.getId());
-//        return "redirect:/dashboard";
-//    }
-//
-//    @GetMapping("/logout")
-//    public String logout(HttpSession session) {
-//        session.invalidate();
-//        return "redirect:/";
-//    }
+    @GetMapping("")
+    public String index(@ModelAttribute("newUser") User newUser, @ModelAttribute("loginUser") LoginUser loginUser) {
+        return "index";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model, HttpSession session) {
+        User user = userService.register(newUser, result);
+
+        if (result.hasErrors()) {
+            model.addAttribute("loginUser", new LoginUser());
+            model.addAttribute("savedDate", newUser.getBirthDate());
+            return "index";
+        }
+        session.setAttribute("userId", user.getId());
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("loginUser") LoginUser loginUser, BindingResult result, Model model, HttpSession session) {
+        if (result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            model.addAttribute("loginUser", loginUser);
+            return "index";
+        }
+
+        User potentialUser = this.userService.login(loginUser, result);
+        if (potentialUser == null) {
+            model.addAttribute("newUser", new User());
+            model.addAttribute("loginUser", loginUser);
+            return "index";
+        }
+
+        session.setAttribute("userId", potentialUser.getId());
+        return "redirect:/dashboard";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
 }
