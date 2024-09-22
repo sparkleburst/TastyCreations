c8df5ca77adc4349c141b59e5644cc152641954b
Irina Markov <imarkov1275@gmail.com>
2024-09-22 07:17:44 -0500

    added Recipe, implemented complex-search

--- src/main/java/com/mgilhespy/tastycreations/services/ApiService.java
+++ src/main/java/com/mgilhespy/tastycreations/services/ApiService.java
@@ -56,6 +56,37 @@ public class ApiService {
     }
 
     @Cacheable("recipes")
+    public Object getRecipes(String query, String cuisine, String diet, String intolerances, String excludeIngredients) throws ApiException {
+//        logger.debug("Fetching recipes with ingredients: {}", ingredients);
+
+        ApiClient apiClient = new ApiClient();
+        apiClient.addDefaultHeader("x-api-key", apiKey);
+        DefaultApi apiInstance = new DefaultApi(apiClient);
+        Object result = null;
+
+//        String query = "burger"; // String | The (natural language) search query.
+//        String cuisine = "italian"; // String | The cuisine(s) of the recipes. One or more, comma separated (will be interpreted as 'OR'). See a full list of supported cuisines.
+//        String diet = "vegetarian"; // String | The diet for which the recipes must be suitable. See a full list of supported diets.
+//        String intolerances = "gluten"; // String | A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
+//        String excludeIngredients = "eggs"; // String | A comma-separated list of ingredients or ingredient types that the recipes must not contain.
+
+        Boolean instructionsRequired = true; // Boolean | Whether the recipes must have instructions.
+        BigDecimal number = BigDecimal.valueOf(10); // BigDecimal | The number of results to return (between 1 and 100).
+        BigDecimal offset = BigDecimal.valueOf(0); // BigDecimal | The number of results to skip (between 0 and 900).
+        Boolean limitLicense = true; // Boolean | Whether the recipes should have an open license that allows display with proper attribution.
+
+
+        try {
+            result = apiInstance.searchRecipes(query, cuisine, diet, excludeIngredients, intolerances, offset, number, limitLicense, instructionsRequired);
+//            logger.debug("Recipes fetched successfully for ingredients: {}", ingredients);
+            logger.debug("Response: {}", result);  // Logs the API response for debugging
+        } catch (ApiException e) {
+            logger.error("Exception when calling DefaultApi#searchRecipes: {}", e.getResponseBody(), e);
+        }
+        return result;
+    }
+
+    @Cacheable("recipes")
     public Object getRecipeInformation(long recipeId, boolean includeNutrition) throws ApiException {
         String url = BASE_URL + recipeId + "/information?includeNutrition=" + includeNutrition + "&apiKey=" + apiKey;
 
