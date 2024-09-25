97e5c2dc9bfa35cf23fbbf620567c52631b2c6bf
sparkleburst <maxandmerlin@gmail.com>
2024-09-25 08:00:24 -0500

    Mock data and added like functionality

---
+++ src/main/java/com/mgilhespy/tastycreations/models/Ingredient.java
@@ -0,0 +1,16 @@
+package com.mgilhespy.tastycreations.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ingredient {
+    private String original;
+    private String name;
+    private String image;
+}
